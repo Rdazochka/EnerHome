1618972494676ef00f4d2816c79f7c477f0efece
Rdazochka <rdazochka@gmail.com>
2026-08-27 17:43:06 +0300

    added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,43 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { IBM_Plex_Serif, Inter, Montserrat, Roboto } from 'next/font/google';
+import './globals.css';
+import type { ReactNode } from 'react';
+
+const montserrat = Montserrat({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-montserrat',
+});
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-inter',
+});
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['500', '700'],
+  variable: '--font-ibm-plex-serif',
+});
+
+const roboto = Roboto({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['700'],
+  variable: '--font-roboto',
+});
 
 export const metadata: Metadata = {
-  title: "EnerHome",
-  description: "Енергоефективні рішення для вашого дому",
+  title: 'EnerHome',
+  description: 'Енергоефективні рішення для вашого дому',
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="uk">
-      <body>{children}</body>
+      <body
+        className={`${montserrat.variable} ${inter.variable} ${ibmPlexSerif.variable} ${roboto.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
